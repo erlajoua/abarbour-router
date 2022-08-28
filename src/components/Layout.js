0ac405c2d05b68fddc956098aea4c7e7ac20f43b
Erwan Lajouannique <erwan.lajouannique@ekimetrics.com>
2022-08-28 15:05:30 +0200

    add nav

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -1,9 +1,13 @@
-import { Outlet } from "react-router-dom"
+import { Outlet, Link } from "react-router-dom"
 
 const Layout = () => {
     return (
         <main className="App">
             <h1>home</h1>
+            
+            <Link to="/login">Sign in Page</Link>
+            <Link to="/register">Sign up Page</Link>
+            <Link to="/linkpage">Link Page</Link>
             <Outlet />
         </main>
     )
